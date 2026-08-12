0cd6eee299f6dea20b01319592e9abb9fedf5173
Adarsha Udupa Baikady <adarsha8505@gmail.com>
2026-08-13 00:50:58 +0530

    Add eBadge firmware

---
+++ eBadge/firmware/display.cpp
@@ -0,0 +1,21 @@
+#include "display.h"
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C
+
+Adafruit_SH1106G display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+bool displayInit() {
+  Wire.begin(5, 6);
+  Wire.setClock(400000);
+
+  if (!display.begin(SCREEN_ADDRESS, true)) {
+    Serial.println("OLED init failed — continuing without display.");
+    return false;
+  }
+  display.clearDisplay();
+  display.display();
+  return true;
+}
